83709600fdbab6f29521eda4a3ffa0a99afa8f8b
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-06-26 19:05:46 +0300

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@
 module.exports = function( grunt ){
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jscs');
 
     grunt.initConfig({
 
@@ -17,5 +16,5 @@ module.exports = function( grunt ){
         }
     });
 
-    grunt.registerTask('default',['jshint','jscs']);
+    grunt.registerTask('default',['jshint']);
 };
